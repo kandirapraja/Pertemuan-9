9791934871eb41153826d2b0edc8b14024181ee0
kandirapraja <kandirasuryandap@gmail.com>
2022-01-08 12:26:41 +0700

    Pertemuan 9

---
+++ ci-news/app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>1st PHP Page</title>
+ </head>
+ <body>
+  <?php echo' <h2>Isi Halaman About</h2>';?>
+ </body>
+</html>
---
+++ ci-news/app/Views/pages/home.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>1st PHP Page</title>
+ </head>
+ <body>
+  <?php echo' <h2>Isi Halaman Home</h2>';?>
+ </body>
+</html>
